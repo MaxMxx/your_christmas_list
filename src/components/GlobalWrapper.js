6654ca20bec8bed1415f523015a8823d6b69f068
MaxMxx <besserve.maxence@gmail.com>
2024-11-22 18:40:51 +0100

    Updates

--- src/components/GlobalWrapper.js
+++
@@ -1,9 +0,0 @@
-// src/GlobalWrapper.js
-import React from "react";
-import "./GlobalWrapper.css";
-
-const GlobalWrapper = ({ children }) => {
-  return <div className="global-wrapper">{children}</div>;
-};
-
-export default GlobalWrapper;
